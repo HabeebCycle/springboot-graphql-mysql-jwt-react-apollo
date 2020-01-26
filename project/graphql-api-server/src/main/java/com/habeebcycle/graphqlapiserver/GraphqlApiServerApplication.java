af0276d0308f1d693fa82b977bce70b1cf4c5bbf
Habeeb Okunade <guessy2k2@yahoo.com>
2020-01-27 09:56:50 +1100

    Dependencies Added

---
+++ project/graphql-api-server/src/main/java/com/habeebcycle/graphqlapiserver/GraphqlApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.habeebcycle.graphqlapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlApiServerApplication.class, args);
+    }
+
+}
---
+++ project/graphql-api-server/src/test/java/com/habeebcycle/graphqlapiserver/GraphqlApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habeebcycle.graphqlapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlApiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
